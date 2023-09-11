21824eaddc3b8fca214b26f3fe1a1ce7e4a0e1b2
aniljos <anil.jos@gmail.com>
2023-09-11 17:17:49 +0530

    Day 1

---
+++ ReactApp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(){
+    return (
+        <div>
+            <h4>React Component</h4>
+            <p>This is a manually configured React Application</p>
+        </div>
+    )
+}
+
+export default App;
